bf27da84a39dc356546fdc44b232e5750a4bd5fd
Navleen <navleenkaurr@gmail.com>
2021-11-09 20:21:03 -0500

    Set up Comments model

---
+++ server/models/Comment.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const CommentSchema = new Schema({
+    comment: {
+        type: String,
+        trim: true
+    },
+    likes: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        }
+    ],
+    date_created: {
+        type: Date,
+        default: Date.now
+    },
+    user_id: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    },
+    post_id: {
+        type: Schema.Types.ObjectId,
+        ref: "Post"
+    }
+});
+
+const Comment = mongoose.model("Comment", CommentSchema);
+
+module.exports = Comment;
