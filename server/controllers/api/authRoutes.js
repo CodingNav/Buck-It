80088dda4296098960f460f6f2324317a77a4231
Navleen <navleenkaurr@gmail.com>
2021-11-10 17:52:52 -0500

    Added jsonwebtoken

--- server/controllers/api/authRoutes.js
+++ server/controllers/api/authRoutes.js
@@ -1,54 +1,5 @@
 const router = require('express').Router();
 const { User } = require('../../models');
-
-// Sign Up
-router.post('/', async (req, res) => {
-  try {
-    const userData = await User.create(req.body);
-
-    req.session.save(() => {
-      req.session.user_id = userData._id;
-      req.session.logged_in = true;
-
-      res.status(200).json(userData);
-    });
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
-
-// Login
-router.post('/login', async (req, res) => {
-    try {
-      const userData = await User.findOne( { $or: [ {email: req.body.username}, {username: req.body.username} ] } );
-  
-      if (!userData) {
-        res
-          .status(400)
-          .json({ message: 'Incorrect email/username or password, please try again' });
-        return;
-      }
-  
-      const validPassword = await userData.checkPassword(req.body.password);
-  
-      if (!validPassword) {
-        res
-          .status(400)
-          .json({ message: 'Incorrect email/username or password, please try again' });
-        return;
-      }
-  
-      req.session.save(() => {
-        req.session.user_id = userData._id;
-        req.session.logged_in = true;
-        
-        res.json({ user: userData, message: 'You are now logged in!' });
-      });
-  
-    } catch (err) {
-      res.status(400).json(err);
-    }
-  });
   
   // Logout
   router.post('/logout', (req, res) => {
