4aed5fd9c29c8cbdeb98a53550b60cb808fbba5a
Navleen <navleenkaurr@gmail.com>
2021-11-10 17:44:24 -0500

    Set up typedefs for user

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,7 +1,32 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    bio: String
+    picture: String
+    followers: []
+    following: []
+    privacy_mode: Boolean
+  }
 
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    users: [User]
+    user(username: String!): User
+  }
+
+  type Mutation{
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(username: String!, password: String!): Auth
+  }
 `;
 
 module.exports = typeDefs;
