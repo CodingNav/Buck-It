59eff2f750ea42b76602cb6fe1ea60945d6275a3
Tiana Bettinson <tiana.bettinson@gmail.com>
2021-11-11 15:29:59 -0500

    Updated mutations and queries with bucketlist, post, and comment information

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -12,3 +12,56 @@ export const GET_ME = gql`
     }
   }
 `;
+
+export const GET_USER = gql`
+  query user($username: String!){
+    user(username: $username){
+      _id
+      username
+      email
+      bio
+      picture
+      bucketList{
+        _id
+        progress
+        # I didn't include post here
+      }
+    }
+  }
+`;
+
+export const GET_BUCKETLISTS = gql `
+  query bucketLists($username: String!){
+    bucketList(username: $username){
+      _id
+      progress
+      post{
+        _id
+        description
+        images
+        likes
+        tags
+        date_created
+        # I did not include the comments here
+      }
+    }
+  }
+`;
+
+export const GET_BUCKETLIST = gql `
+  query bucketList($listId: ID!){
+    bucketList(listId: $listId){
+      _id
+      progress
+      post{
+        _id
+        description
+        images
+        likes
+        tags
+        date_created
+        # I did not include the comments here
+      }
+    }
+  }
+`;
