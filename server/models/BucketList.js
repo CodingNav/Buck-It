464883d243f876c274c4695e10a9ab782ad46a6d
Navleen <navleenkaurr@gmail.com>
2021-11-12 12:19:00 -0500

    Updated BucketList.js

--- server/models/BucketList.js
+++ server/models/BucketList.js
@@ -1,21 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const {PostSchema} = require('./Post');
 
 const Schema = mongoose.Schema;
 
 const BucketListSchema = new Schema({
-  name: {
-    type: String,
-    trim: true,
-  },
-  progress: {
-    type: String,
-  },
-  user_id: {
-    type: Schema.Types.ObjectId,
-    ref: 'User',
-  },
+    name: {
+        type: String,
+        trim: true
+    },
+    progress: {
+        type: String,
+    },
+    post: [PostSchema]
 });
 
 const BucketList = mongoose.model('BucketList', BucketListSchema);
 
-module.exports = BucketList;
+module.exports = {BucketList, BucketListSchema};
