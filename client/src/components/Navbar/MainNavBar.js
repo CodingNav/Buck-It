74ec65fcec2d20fc531dae59a7845dda53a63d99
Mehdi Mehrabani <mmehr1988@gmail.com>
2021-11-18 15:41:25 -0500

    staging my commits for main merge

--- client/src/components/Navbar/MainNavBar.js
+++ client/src/components/Navbar/MainNavBar.js
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Auth from '../../utils/auth';
+
+import './Navbar.css';
 // NAV Links
 import LoginSignUp from './LoginSignUpModal';
 import Logout from './Logout';
 import Profile from './Profile';
 import Search from './Search';
+import RainDrop from '../../utils/animation';
 
 // Bootstrap Navbar
-import { Navbar, Nav, Container, Stack, Row, Col, Form, FormControl, Button } from 'react-bootstrap';
+import { Navbar, Nav, Container, Stack, Row, Col } from 'react-bootstrap';
 import { BucketFill } from 'react-bootstrap-icons';
 import { LinkContainer } from 'react-router-bootstrap';
 
 const MainNavBar = () => {
+  const [show, setShow] = useState(false);
+
+  const toggle = () => {
+    setShow((show) => !show);
+  };
+
   return (
     <>
+      <RainDrop showAnimation={show} />
       <Container fluid>
         <Row>
           <Col>
@@ -30,7 +40,7 @@ const MainNavBar = () => {
               <LinkContainer to='/'>
                 <Navbar.Brand href='/' className='p-2'>
                   <Stack direction='horizontal' gap={1}>
-                    <BucketFill size={36} />
+                    <BucketFill size={36} className='BucketBrand' onMouseOver={() => toggle()} onMouseOut={() => toggle()} />
                     <h1
                       className='mb-0 mt-2'
                       style={{
