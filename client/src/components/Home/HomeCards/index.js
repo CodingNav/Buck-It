bc60b30844e58ed984afb21cb30c9d86d11ed14d
Navleen <navleenkaurr@gmail.com>
2021-11-19 13:23:39 -0500

    Set up homepage cards

--- client/src/components/Home/HomeCards/index.js
+++ client/src/components/Home/HomeCards/index.js
@@ -1,42 +1,56 @@
 import React from 'react';
-
 import { Col, Card, Image } from 'react-bootstrap';
 
+import { useQuery } from '@apollo/client';
+import { GET_ALL_POSTS } from '../../../utils/queries';
+import { convertDate } from '../../../utils/dateConvert';
+
 const HomeCards = () => {
+
+  const { loading, data } = useQuery(GET_ALL_POSTS);
+  console.log(data);
+  if (loading) {
+    return null;
+  }
   return (
     <>
-      <Col className='pb-2' sm={4} md={4} lg={4}>
-        <Card className='shadow h-100'>
-          {/* CARD HEADER */}
-          <Card.Title>
-            <div className='BuckitCardTitleContainer'>
-              <div className='BuckitCardImageUsername'>
-                <Image className='BuckitCardProfileImage' src='https://source.unsplash.com/XHVpWcr5grQ/60x60' roundedCircle />
-                <p className='BuckitCardUsername '>mmehr1988</p>
+      {data.getAllPosts.map((post) => (
+        <Col className='pb-2' sm={4} md={4} lg={4}>
+
+          <Card className='shadow h-100'>
+            {/* CARD HEADER */}
+            <Card.Title>
+              <div className='BuckitCardTitleContainer'>
+                <div className='BuckitCardImageUsername'>
+                  <Image className='BuckitCardProfileImage' src={post.createdBy.picture} width= "60px" roundedCircle />
+                  <p className='BuckitCardUsername '>{post.createdBy.username}</p>
+                </div>
+                <div className='BuckitCardDate'>{convertDate(post.date_created)}</div>
               </div>
-              <div className='BuckitCardDate'>Nov 14,2021</div>
-            </div>
-          </Card.Title>
-          {/* CARD BODY */}
-          <Card.Body className='BuckitCardBodyContainer'>
-            <div className='BuckitCardBodyTitle'>I visited the pyramids of Giza!</div>
-            <Card.Img className='BuckitCardImage rounded' src='https://source.unsplash.com/CJ4mbwSK3EY' />
-            <div className='BuckitCardBodyDescription'>This summer, I visited the pyramids of Giza! They were incredible!</div>
-          </Card.Body>
+            </Card.Title>
+            {/* CARD BODY */}
+            <Card.Body className='BuckitCardBodyContainer'>
+              <div className='BuckitCardBodyTitle'>{post.title}</div>
+              <Card.Img className='BuckitCardImage rounded' src='https://source.unsplash.com/CJ4mbwSK3EY' />
+              <div className='BuckitCardBodyDescription'>{post.description}</div>
+            </Card.Body>
 
-          <div className='BuckitCardBodyStatus'>
-            Status: <span>In Progress</span>
-          </div>
-
-          {/* HEADER /////////////// ROW #6 */}
-          <Card.Footer className='bg-transparent'>
-            <div className='BuckitCardFooter'>
-              <div>#Travel</div>
-              <div>#World</div>
+            <div className='BuckitCardBodyStatus'>
+              Status: <span>In Progress</span>
             </div>
-          </Card.Footer>
-        </Card>
-      </Col>
+
+            {/* HEADER /////////////// ROW #6 */}
+            <Card.Footer className='bg-transparent'>
+              <div className='BuckitCardFooter'>
+                {post.tags.map((tag) => (
+                  <div>#{tag}</div>
+                ))}
+              </div>
+            </Card.Footer>
+          </Card>
+
+        </Col>
+      ))}
     </>
   );
 };
