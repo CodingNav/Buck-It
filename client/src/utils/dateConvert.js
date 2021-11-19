2c7cc1ca25fb78e0d4591d4ebf35e09b17bf7c98
Navleen <navleenkaurr@gmail.com>
2021-11-19 13:24:12 -0500

    Fixed date converter

--- client/src/utils/dateConvert.js
+++ client/src/utils/dateConvert.js
@@ -1,5 +1,5 @@
 export const convertDate = (date) => {
-  const d = new Date(date);
+  const d = new Date(parseInt(date));
   const ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(d);
   const mo = new Intl.DateTimeFormat('en', { month: 'short' }).format(d);
   const da = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(d);
