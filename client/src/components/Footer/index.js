19f0cabba9c3b37878a17c30740211fc02a61eb7
Mehdi Mehrabani <mmehr1988@gmail.com>
2021-11-18 20:25:11 -0500

    commit before main merge

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { Container } from 'react-bootstrap';
+
+const Footer = () => {
+  return (
+    <>
+      <div className='rounded'>
+        <Container className='pb-2 bg-dark' fluid>
+          <div>Hello</div>
+        </Container>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
